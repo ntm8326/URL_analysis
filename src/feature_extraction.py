3189711e3209ca0ce8f0ac2f7bb804bb75481eec
ntm8326 <ntm8326@gmail.com>
2026-05-19 20:52:34 +0700

    extract url ver2

--- src/feature_extraction.py
+++ src/feature_extraction.py
@@ -1,3 +1,4 @@
+import re
 from urllib.parse import urlparse
 import tldextract
 
@@ -5,22 +6,31 @@ def url_length(url):
     return len(url)
 
 def extract_url(url):
-    parsed_url = urlparse(url)
+    url = str(url).strip()
+    url = re.sub(r'([a-zA-Z0-9\-\+\.]+:(?!//))[/]*', r'\1//', url)
     extracted = tldextract.extract(url)
-    if len(parsed_url.scheme) == 0:
-        url = "http://" + url
-        return extract_url(url)
-    parts_array = [
-        parsed_url.scheme,
-        parsed_url.netloc,
-        extracted.subdomain,
-        extracted.domain,
-        extracted.suffix,
-        parsed_url.path,
-        parsed_url.query
-    ]
-    return parts_array
+    parsed_origin = urlparse(url)
+    if not parsed_origin.scheme and not url.startswith("//"):
+        temp_url = "//" + url
+        parsed_working = urlparse(temp_url)
+    else:
+
+        parsed_working = parsed_origin
+    parts = [parsed_origin.scheme,
+             parsed_working.netloc,
+             extracted.subdomain,
+             extracted.domain,
+             extracted.suffix,
+             parsed_working.path,
+             parsed_working.params,
+             parsed_working.query,
+             parsed_working.fragment
+             ]
+    return parts
+
 
 url1 = "facebook.com"
 url2 = "http://www.ikenmijnkunst.nl/index.php/exposities/exposities-2006"
-print(extract_url(url1))
+url3 ="http://www.habbocreditos4.t83.net/#/hc-y-vip/4553149448"
+url4 = "http:faceb00k.com"
+print(extract_url(url4))
